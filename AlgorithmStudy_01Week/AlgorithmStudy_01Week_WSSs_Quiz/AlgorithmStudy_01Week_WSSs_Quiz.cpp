4510e72f39b8a497a955ac989292cbd428b7e6e5
wusubsin <mainmethod0126@gmail.com>
2019-01-15 17:01:08 +0900

    Title(Type:Feat) KOR : 정렬된 2차원 벡터 두 라인씩 라인벡터의 원소를 서로 비교하는 코드 추가 및 알고리즘 문제 완성
    Body
    KOR : 상세 내용은 코드 주석 참조

--- AlgorithmStudy_01Week/AlgorithmStudy_01Week_WSSs_Quiz/AlgorithmStudy_01Week_WSSs_Quiz.cpp
+++ AlgorithmStudy_01Week/AlgorithmStudy_01Week_WSSs_Quiz/AlgorithmStudy_01Week_WSSs_Quiz.cpp
@@ -19,25 +19,39 @@ int main()
 */
 	//std::vector<std::vector<int> > arr(nSizeY, std::vector<int>(nSizeX, 0));
 
+
 	// 2019-01-14 wssin
 	// 이차원 벡터 선언 및 초기화
 	// 솔루션 함수 검증을 위한 입력 값 셋팅.
-	std::vector< std::vector<int> > nVectInput{
-		{5 ,0, 0, 0, 0, 0},
-		{1, 4, 0, 0, 0, 0},
-		{4, 3, 3, 2, 1, 0},
+	// 입력 예시 1번
+	std::vector< std::vector<int> > nVectInput1{
+		{5},
+		{1, 4},
+		{4, 3, 3, 2, 1},
 		{5, 2, 4, 3, 2, 1},
-		{4, 4, 3, 3, 1, 0},
-		{4, 3, 2, 1, 1, 0},
-		{4, 2, 3, 2, 1, 0},
-		{4, 4, 3, 2, 1, 0},
-		{3, 4, 3, 2, 0, 0},
+		{4, 4, 3, 3, 1},
+		{4, 3, 2, 1, 1},
+		{4, 2, 3, 2, 1},
+		{4, 4, 3, 2, 1},
+		{3, 4, 3, 2},
 		{5, 4, 4, 2, 3, 1},
 		{5, 4, 2, 4, 1, 3}
 	};
 
-	solution(nVectInput);
+	// 입력 예시 2번
+	std::vector< std::vector<int> > nVectInput2{
+		{4},
+		{4, 4, 3, 2, 1},
+		{4, 1, 4, 3, 2},
+		{4, 3, 3, 2, 1},
+		{4, 4, 3, 3, 3},
+		{4, 4, 3, 3, 3},
+		{4, 3, 4, 3, 2},
+		{4, 3, 2, 1, 1},
+		{3, 3, 2, 1}
+	};
 
+	solution(nVectInput2);
 }
 
 void solution(std::vector<std::vector<int>>& nVectInput)
@@ -46,17 +60,82 @@ void solution(std::vector<std::vector<int>>& nVectInput)
 	// v[0][0]을 제외한 v[1][0] 부터 시작하여
 	// v[N][1]부터 v[N][Length] 까지 정렬
 	// std::sort는 퀵소트 기반
-	for (int AxisY = 1; AxisY <= nVectInput[0][0]; AxisY++)
+	for (int AxisY = 1; AxisY <= nVectInput[0][0] * 2; AxisY++)
 	{
 		std::sort(nVectInput[AxisY].begin() + 1, nVectInput[AxisY].end(), std::greater<int>());
 	}
 
-	// 더미 코드
-	int a = 10;
-
 	// 정렬이 완료되었으니 이제 비교문 추가하면 됨.
+	for (int AxisY = 1; AxisY <= nVectInput[0][0] * 2; AxisY += 2)
+	{
+		int nIter = 0;
+
+		// 2019-01-15 wssin
+		// 비교할 두 열의 길이를 비교하여 작은놈으로 반복자 선택
+		// 이유. 한놈이 더 길거나 짧을 경우 한놈의 길이를 AxisX로하여 비교하면 널포인트 익셉션 발생
+		// 그렇기에 작은놈으로 비교하고 어차피 작은 놈의 마지막 까지 비교했는데
+		// 아직 게임이 안끝났으면 작은놈이 진거임.
+		// 예를 들어
+		// A : 4, 3, 3, 2, 1
+		// B : 4, 3, 3, 2
+		// 두 입력에서
+		// 길이가 작은 B의 길이를 기준으로 비교횟수를 설정하고 비교하였을 시
+		// B의 마지막인 4번째 까지 비교했는대도 동점이다.
+		// A의 나머지 숫자가 뭔지 생각할 필요도 없지
+		nVectInput[AxisY][0] >= nVectInput[AxisY + 1][0] ? nIter = nVectInput[AxisY + 1][0] : nIter = nVectInput[AxisY][0];
+
+		for (int AxisX = 1; AxisX <= nIter; AxisX++)
+		{
+			// A가 크면
+			if (nVectInput[AxisY][AxisX] > nVectInput[AxisY + 1][AxisX])
+			{
+				std::cout << "A\n";
+				break;
+			}
+			
+			// B가 크면
+			else if (nVectInput[AxisY][AxisX] < nVectInput[AxisY + 1][AxisX])
+			{
+				std::cout << "B\n";
+				break;
+			}
+
+			// 마지막까지 돌았는데도 승패가 갈리지 않았다면
+			if (AxisX == nIter)
+			{
+				// 2019-01-15 wssin
+				// 각 라인 0번 인덱스는 그 라인의 도형 갯수
+				// 그러니까 아직 승패가 갈리지 않았다면
+				// 누가 더 큰지 비교해봐서
+				// 더 큰놈이 이기고
+				// 만약 크기가 같다면
+				// 드로우
+				if (nVectInput[AxisY][0] > nVectInput[AxisY + 1][0])
+				{
+					std::cout << "A\n";
+					break;
+				}
+				else if (nVectInput[AxisY][0] < nVectInput[AxisY + 1][0])
+				{
+					std::cout << "B\n";
+					break;
+				}
+				else
+				{
+					std::cout << "D\n";
+					break;
+				}
+			}
+		}
+	}
 }
 
+
+/*
+	2019-01-15 wssin
+	영단어 외워야하는데... 내일 토익학원 시험인대..
+	안외웠다고 또 혼나겠다..
+*/
 
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
